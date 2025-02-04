8244593d1e57b08cabd4af27e3b645e6dae78c03
Aditya Mathpal <adimathpal@gmail.com>
2025-02-04 18:17:39 +0530

    lab 4 complete

--- Lab 04/hex_to_packed.s
+++ Lab 04/hex_to_packed.s
@@ -9,15 +9,31 @@ __Vectors
     ENTRY
     EXPORT Reset_Handler
 
-Reset_Handler
-    ldr r0, =hex
-    ldr r1, [r0]
-    
-loop
-    
-    bne loop
-    ldr r0, =bcd
-    str r2, [r0]
+Reset_Handler 
+	ldr r5, =hex
+	ldr r0, [r5]
+    mov r1, #0 
+    mov R2, #0 
+    mov R3, #0 
+ 
+x 
+    cmp R0, #10 
+    bcc xx 
+    sub R0, R0, #10 
+    add R2, R2, #1 
+    b x 
+ 
+xx 
+    mov R4, R0, LSL R3 
+    add R3, R3, #4
+    orr R1, R1, R4 
+    mov R0, R2
+    cmp R0, #0 
+	ldr r5, =bcd
+	str r1, [r5]
+    beq stop
+    mov R2, #0 
+	b x
 stop b stop
 
 	AREA mydata, DATA, READONLY
