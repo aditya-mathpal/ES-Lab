d89fea2e075af937923f7a3e6b519ce4af4ef5ef
Aditya Mathpal <adimathpal@gmail.com>
2025-02-04 21:15:22 +0530

    lab 4 complete

--- Lab 04/hex_to_packed.s
+++ Lab 04/hex_to_packed.s
@@ -13,26 +13,26 @@ Reset_Handler
 	ldr r5, =hex
 	ldr r0, [r5]
     mov r1, #0 
-    mov R2, #0 
-    mov R3, #0 
+    mov r2, #0 
+    mov r3, #0 
  
 x 
-    cmp R0, #10 
+    cmp r0, #10 
     bcc xx 
-    sub R0, R0, #10 
-    add R2, R2, #1 
+    sub r0, r0, #10 
+    add r2, r2, #1 
     b x 
  
 xx 
-    mov R4, R0, LSL R3 
-    add R3, R3, #4
-    orr R1, R1, R4 
-    mov R0, R2
-    cmp R0, #0 
+    mov r4, r0, LSL r3 
+    add r3, r3, #4
+    orr r1, r1, r4 
+    mov r0, r2
+    cmp r0, #0 
 	ldr r5, =bcd
 	str r1, [r5]
     beq stop
-    mov R2, #0 
+    mov r2, #0 
 	b x
 stop b stop
 
@@ -45,6 +45,12 @@ bcd dcd 0
 
 /*
 output:
-
-hex: addr: 0x00000048 val: 
-bcd: addr: 0x10000000 val: 0x033337
+R0: 0x00000000
+R1: 0x00030538
+R2: 0x00000000
+R3: 0x00000014
+R4: 0x00030000
+R5: 0x10000000
+hex: addr: 0x0000004C val: 0x774A
+bcd: addr: 0x10000000 val: 0x030538
+*/
