f8d6353083ed2320dc1ae964cc88167ae36932b1
Aditya Mathpal <adimathpal@gmail.com>
2025-02-12 11:45:34 +0530

    update

--- Lab 06/selection_sort.s
+++ Lab 06/selection_sort.s
@@ -18,7 +18,7 @@ Reset_Handler
 	mov r1, #10
 l	ldr r3, [r0, r2] ; load number from src
 	str r3, [r4, r2] ; store number to dst
-	add r2, #4 ; r0+=4
+	add r2, #1 ; r2+=1
 	sub r1, #1 ; r1--
 	cmp r1, #0
 	bhi l ; repeat till all numbers are transferred from src to dst
@@ -37,19 +37,19 @@ x	mov r7, r6
 	cmp r5, r8
 	movhi r3, r7
 	add r2, #1
-	cmp r2, #5
+	cmp r2, #10
 	bne x
 	ldrb r9, [r3]
 	ldrb r10, [r4]
 	strb r9, [r4]
 	strb r10, [r3]
 	add r1, #1
-	cmp r1, #4
+	cmp r1, #9
 	bne xx
 
-arr dcd 0x10, 0x05, 0x33, 0x24, 0x56, 0x77, 0x21, 0x04, 0x87, 0x01
+arr dcb 0x10, 0x05, 0x33, 0x24, 0x56, 0x77, 0x21, 0x04, 0x87, 0x01
 	AREA mydata, data, readwrite
-dst dcd 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+dst dcb 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	
 	end
 
